dd55841c9255cf9569331b31c478bb9ba7fdc951
s-baltar <32884123+s-baltar@users.noreply.github.com>
2020-11-03 14:18:17 -0800

    Initial commit

---
+++ app/src/main/java/com/spotifypgen/Connectors/VolleyCallBack.java
@@ -0,0 +1,5 @@
+package com.spotifypgen.Connectors;
+
+public interface VolleyCallBack {
+    void onSuccess();
+}
---
+++ app/src/main/java/com/spotifypgen/VolleyCallBack.java
@@ -0,0 +1,5 @@
+package com.spotifypgen;
+
+public interface VolleyCallBack {
+    void  onSuccess();
+}
