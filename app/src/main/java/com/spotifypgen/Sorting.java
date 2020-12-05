97e7ac93b8731b3a43b9161cfcfd754f0627af09
s-baltar <32884123+s-baltar@users.noreply.github.com>
2020-12-05 13:22:05 -0800

    Remove unused functions of Sorting and add comments

--- app/src/main/java/com/spotifypgen/Sorting.java
+++ app/src/main/java/com/spotifypgen/Sorting.java
@@ -1,7 +1,6 @@
 package com.spotifypgen;
 
 import android.os.Build;
-import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
@@ -9,12 +8,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.logging.Level;
-
-import static java.lang.Math.pow;
-import static java.lang.Math.random;
-import static java.lang.Math.sqrt;
-
 
 public class Sorting {
 
@@ -30,9 +23,6 @@ public class Sorting {
 
     private HashMap<Feat, Double> featurePref = new HashMap<Feat, Double>();
     private ArrayList<Features> features = new ArrayList<>();
-    private boolean isSavedTracks = false;
-
-    final double DISTANCE_CUTTOFF = 4;
 
     public Sorting() {
         featurePref.put(Feat.DANCEABILITY, 0.5);
@@ -53,13 +43,8 @@ public class Sorting {
         else if (audioFeature == Feat.LOUDNESS) {
             double db = value; // TODO: Scale from [0,100] to [-60,0] dB
             featurePref.put(audioFeature, db);
-        } else
+        } else // Scale vale down by 100.
             featurePref.put( audioFeature, value / 100 );
-    }
-
-    // Get desired user preferred audio feature.
-    public double setFeaturePreferences(Feat audioFeature) {
-        return featurePref.get(audioFeature);
     }
 
     //  Info: Get desired user preferred audio feature.
@@ -73,31 +58,6 @@ public class Sorting {
         features = f;
     }
 
-    //  Info: Calculate distance between user preferred features and given features of a song.
-    //   Ret: Double value of distance.
-    private double calcDistance(Features point) {
-        double sqrtArg = pow(featurePref.get(Feat.DANCEABILITY)     - point.getDanceability(), 2) +
-                         pow(featurePref.get(Feat.ENERGY)           - point.getDanceability(), 2) +
-                         pow(featurePref.get(Feat.LOUDNESS)         - point.getLoudness(), 2) +
-                         pow(featurePref.get(Feat.ACCOUSTICNESS)    - point.getAcousticness(), 2) +
-                         pow(featurePref.get(Feat.INSTRUMENTALNESS) - point.getInstrumentalness(), 2) +
-                         pow(featurePref.get(Feat.VALENCE)          - point.getValence(), 2);
-
-        return sqrt(sqrtArg);
-    }
-
-    //  Info: Sort distances in ascending. Only to be used for saved tracks.
-    // Param: preferences - User's desired audio features.
-    //   Ret: ArrayList<Features> - features that are closest to preferences.
-    public void sortDistance() {
-
-        // Calculate all song 'distances'
-        for (int i = 0; i < features.size(); i++)
-            features.get(i).setDistance(calcDistance(features.get(i)));
-
-        // Sort in ascending order.
-        Collections.sort(features, new Sortbydistance());
-    }
 
     //  Info: Filters out songs that exceed a certain 'distance'.
     //        Distributes songs into bucket levels based on the songs tempo.
@@ -109,53 +69,47 @@ public class Sorting {
     public ArrayList<Features> sortBuckets() {
 
         ArrayList<Features> bucket = new ArrayList<Features>();
-        ArrayList<Features> bucket_high = new ArrayList<Features>();
-        ArrayList<Features> bucket_moderate = new ArrayList<Features>();
-        ArrayList<Features> bucket_low = new ArrayList<Features>();
-        ArrayList<Features> gen_playlist = new ArrayList<Features>();
+        ArrayList<Features> bucket_high = new ArrayList<Features>();        // High tempo songs
+        ArrayList<Features> bucket_moderate = new ArrayList<Features>();    // Moderate tempo songs
+        ArrayList<Features> bucket_low = new ArrayList<Features>();         // Low tempo songs
+        ArrayList<Features> gen_playlist = new ArrayList<Features>();       // Final list of sorted songs.
 
-        if (isSavedTracks) {
-            // Filter out songs
-            for (int i = 0; i < features.size(); i++) {
-                if (features.get(i).getDistance() > DISTANCE_CUTTOFF)
-                    break;
-                bucket.add(features.get(i));
-            }
-        } else // Otherwise use given features
-            bucket = features;
+        bucket = features;
 
-        Collections.sort(bucket, new Sortbytempo());
+        Collections.sort(bucket, new Sortbytempo()); // Sort bucket by tempo.
+
+        // Distribute songs into buckets.
         int tenthOfBucket = (int) (bucket.size()/10);
-
-        for (int i=0; i<tenthOfBucket*2 ; i++)
+        for (int i=0; i<tenthOfBucket*2 ; i++)  // Add top 20% songs to bucket.
             bucket_high.add(bucket.get(i));
 
-        for (int i=tenthOfBucket*2; i< tenthOfBucket*4; i++)
+        for (int i=tenthOfBucket*2; i< tenthOfBucket*4; i++) // Add top 20%-40% songs to bucket.
             bucket_moderate.add(bucket.get(i));
 
-        for (int i=tenthOfBucket*4; i< bucket.size(); i++)
+        for (int i=tenthOfBucket*4; i< bucket.size(); i++) // Add bottom 60% songs to bucket.
             bucket_low.add(bucket.get(i));
 
-        int duration = (int)(featurePref.get(Feat.LENGTH) * (60 * 1000));
+        int duration = (int)(featurePref.get(Feat.LENGTH) * (60 * 1000)); // min to ms
 
 
         int randomNum = 0;
         int i = bucket_moderate.get(randomNum).getDuration_ms();
         do { // Add moderate tempo songs to playlist
 
-            gen_playlist.add(bucket_moderate.get(randomNum));
+            gen_playlist.add(bucket_moderate.get(randomNum)); // Add song to final sorted list.
 
-            bucket_moderate.remove(randomNum);
+            bucket_moderate.remove(randomNum);  // Remove, so no repeated songs.
 
-            if (bucket_moderate.size() <= 0)
+            if (bucket_moderate.isEmpty())
                 break;
 
+            // Generate randomNum for index of bucket.
             randomNum = (bucket_moderate.size() > 1)
                     ? ThreadLocalRandom.current().nextInt(0, bucket_moderate.size() - 1)
                     : 0;
 
-            i += bucket_moderate.get(randomNum).getDuration_ms();
-        } while (i<=duration/5);
+            i += bucket_moderate.get(randomNum).getDuration_ms(); // Increment current playlist length.
+        } while (i<=duration/5); // While current length of playlist <= fifth of desired duration
 
 
         randomNum = 0;
@@ -165,27 +119,27 @@ public class Sorting {
 
             bucket_high.remove(randomNum);
 
-            if (bucket_high.size() <= 0)
+            if (bucket_high.isEmpty())
                 break;
 
             randomNum = (bucket_high.size() > 1)
                     ? ThreadLocalRandom.current().nextInt(0, bucket_high.size() - 1)
                     : 0;
             i += bucket_high.get(randomNum).getDuration_ms();
-        } while (i<=duration/2);
+        } while (i<=duration/2); // While current bucket duration <= half of desired duration
 
 
         randomNum = 0;
-        i = (bucket_moderate.size() > 0) ? bucket_moderate.get(randomNum).getDuration_ms() : -1;
+        i = (!bucket_moderate.isEmpty()) ? bucket_moderate.get(randomNum).getDuration_ms() : -1;
         do { // Add moderate tempo songs to playlist
-            if (bucket_moderate.size() <= 0)
+            if (bucket_moderate.isEmpty())
                 break;
 
             gen_playlist.add(bucket_moderate.get(randomNum));
 
             bucket_moderate.remove(randomNum);
 
-            if (bucket_moderate.size() <= 0)
+            if (bucket_moderate.isEmpty())
                 break;
 
             randomNum = (bucket_moderate.size() > 1)
@@ -203,7 +157,7 @@ public class Sorting {
 
             bucket_low.remove(randomNum);
 
-            if (bucket_low.size() <= 0)
+            if (bucket_low.isEmpty())
                 break;
 
             randomNum = (bucket_low.size() > 1)
@@ -211,16 +165,16 @@ public class Sorting {
                     : 0;
 
             i += bucket_low.get(randomNum).getDuration_ms();
-        } while (i<=duration/10);
+        } while (i<=duration/10); // While current bucket duration <= tenth of desired duration
 
         return gen_playlist;
     }
 
-    // Sorts songs based on audio features.
+    //  Info: Sorts songs based on audio features.
+    // Param: f - list of songs and their features to be sorted.
+    //   Ret: List of songs that increase and then decrease in tempo.
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public ArrayList<Features> sortFeatures(ArrayList<Features> f) {
-        if (isSavedTracks)
-            sortDistance();
         setFeatures(f);
         return sortBuckets();
     }
